077498dda7fbcb3df8d83872b7d64bd3e6bd25d7
elias <eliasfaculdade@yahoo.com.br>
2025-07-10 17:33:41 -0300

    ajustes

--- teste/obj/Debug/net8.0/teste.AssemblyInfo.cs
+++ teste/obj/Debug/net8.0/teste.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("teste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+674d738c3cb52bcbbdf49550a5885913c5209924")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edb014094ec8ec1402ba60153a6b4039b88c4be9")]
 [assembly: System.Reflection.AssemblyProductAttribute("teste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("teste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
